af8a71af53ac30ae66f440eb9314cc57d1ca9997
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-11-02 23:37:36 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc = "https://avatars.githubusercontent.com/u/25058652";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}
+        mt="12">
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            Welcome to CoinTrack360 - Your Trusted Source for Cryptocurrency Information.
+            CoinTrack360 is to provide you with accurate, up-to-date, and comprehensive information about the world of cryptocurrencies.
+
+            Go to coins section to check details for each Cryptocurrency listed there.
+
+          </Text>
+        </VStack>
+
+        
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
