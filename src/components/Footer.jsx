f17af357a69535326db2c364136632a9f57a8759
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-11-03 19:51:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,5 @@
-import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import { Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
-
-const avatarSrc = "https://avatars.githubusercontent.com/u/25058652";
 
 const Footer = () => {
   return (
